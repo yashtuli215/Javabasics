324f2946bfe856288e42d324467d0ece4f577ab6
yashtuli215 <60575658+yashtuli215@users.noreply.github.com>
2022-05-24 21:21:03 +0530

    Add files via upload

---
+++ starpattern.java
@@ -0,0 +1,16 @@
+package com.yash;
+
+public class starpattern {
+    public static void main(String[] args) {
+        for (int i =5;i>0;i--){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+
+        }
+
+    }
+}
